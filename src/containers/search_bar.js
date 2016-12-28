698e8f045fd3d57bcfcff52ca0fe83804e53cdc7
DumeD <daviddume3@gmail.com>
2016-12-28 22:28:58 +0100

    first commit

---
+++ src/containers/search_bar.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+    this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+  }
+  onInputChange(event) {
+    this.setState({ term: event.target.value });
+  }
+  onFormSubmit(event) {
+    event.preventDefault();
+
+    this.props.fetchWeather(this.state.term);
+    this.setState({ term: '' }); // clear the input
+  }
+  render() {
+    return (
+      <form onSubmit={this.onFormSubmit} className='input-group'>
+        <input
+          placeholder='Search for city'
+          className='form-control'
+          value={this.state.term}
+          onChange={this.onInputChange}
+        />
+        <span className='input-group-btn'>
+          <button type='submit' className='btn btn-secondary'>Submit</button>
+        </span>
+      </form>
+    );
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+// we use null beacuse we don't need to useany state from redux
+export default connect(null, mapDispatchToProps)(SearchBar);
---
+++ src/reducers/reducer_weather.js
@@ -0,0 +1,10 @@
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function(state = [], action) {
+  switch (action.type) {
+    case FETCH_WEATHER:
+    // using concat to not change state with push()
+      return state.concact([action.payload.data]);
+  }
+  return state;
+}
